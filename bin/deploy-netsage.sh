0f460971b237244e1ef285f9aaa043c306dd5ce6
Charles Kloote <ckloote@gmail.com>
2017-09-08 18:44:22 +0000

    fixed bug in script

--- bin/deploy-netsage.sh
+++ bin/deploy-netsage.sh
@@ -1,2 +1,2 @@
 /usr/lib/perfsonar/bin/build_json -input conf/netsage-mesh.conf -output netsage-mesh.json && 
-sudo cp netsage-mesh.json /var/www/html/meshes/netsage-mesh.json && 
+sudo cp netsage-mesh.json /var/www/html/meshes/netsage-mesh.json
