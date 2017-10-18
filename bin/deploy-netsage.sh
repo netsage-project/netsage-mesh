15ac4c715cf47d78b3dc14a6f6259b1b5f78ccf4
Michael Johnson <mj82@grnoc.iu.edu>
2017-10-18 17:45:15 +0000

    Add lon/fra hosts to NEAAR mesh

--- bin/deploy-netsage.sh
+++ bin/deploy-netsage.sh
@@ -1,2 +1,3 @@
 /usr/lib/perfsonar/bin/build_json -input conf/netsage-mesh.conf -output netsage-mesh.json && 
 sudo cp netsage-mesh.json /var/www/html/meshes/netsage-mesh.json
+sudo service perfsonar-meshconfig-guiagent restart
